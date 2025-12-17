dde488afb29bd53bfd9e2cae5736bbff6a33985f
MustardMike <mike@socialcube.co.uk>
2025-12-17 11:01:54 +0000

    first commit

---
+++ components/hello.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const Hello = () => {
+    console.log('I am a client component')
+    return (
+        <>
+            <h2>
+                Hello
+            </h2>
+        </>
+    )
+}
+export default Hello;
